6064e5531b795a512fc9f9c0979590e49bf8480d
Magesh <tkmagesh77@gmail.com>
2018-07-02 15:03:13 +0530

    calculator - solution

--- 01-Language/src/calculator.js
+++ 01-Language/src/calculator.js
@@ -1,3 +1,20 @@
 function add(x,y){
-	return x + y;
+	function parseArg(n){
+		if (Array.isArray(n)) return add.apply(this, n);
+		if (typeof n === 'function') return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n,10);
+	}
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
 }
+
+/*
+As a method ( of obj )
+	this -> obj
+
+As a function
+	this -> global (window)
+
+using the 'call' method of the function
+
+using the 'apply' method of the function
+*/
