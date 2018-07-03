7105338d39cf1f5da82ad322e327f8744b71b9c8
Magesh <tkmagesh77@gmail.com>
2018-07-03 11:32:29 +0530

    filter assignment

--- 03-FP/products.js
+++ 03-FP/products.js
@@ -22,29 +22,118 @@ describe('Sort', function(){
 	
 	describe('Default sort [ products by id ]', function(){
 		function sort(){
-			//sort the products by id
+			for(var i=0; i < products.length-1; i++)
+				for(var j=i+1; j < products.length; j++)
+					if (products[i].id > products[j].id){
+						var temp = products[i];
+						products[i] = products[j];
+						products[j] = temp;
+					}
 		}
 		sort();
 		console.table(products);	
 	});
-	
-	/*describe('Sorting by Id', function(){
-		//sort();
-		console.table(products)
+
+	describe('Any list by any attribute', function(){
+		function sort(list, attrName){
+			for(var i=0; i < list.length-1; i++)
+				for(var j=i+1; j < list.length; j++)
+					if (list[i][attrName] > list[j][attrName]){
+						var temp = list[i];
+						list[i] = list[j];
+						list[j] = temp;
+					}
+		}
+		describe('Products by cost', function(){
+			sort(products, 'cost');
+			console.table(products);
+		});
+		describe('Products by units', function(){
+			sort(products, 'units');
+			console.table(products);
+		})
 	});
-	describe('Sorting by Cost', function(){
-		//sort();
-		console.table(products)
-	});*/
+
+	describe('Any list by any comparer', function(){
+		function sort(list, comparerFn){
+			for(var i=0; i < list.length-1; i++)
+				for(var j=i+1; j < list.length; j++)
+					if (comparerFn(list[i], list[j]) > 0){
+						var temp = list[i];
+						list[i] = list[j];
+						list[j] = temp;
+					}
+		}
+		describe('Products by value [cost * units]', function(){
+			function productComparerByValue(p1, p2){
+				var p1Value = p1.cost * p1.units,
+					p2Value = p2.cost * p2.units;
+				if (p1Value < p2Value) return -1;
+				if (p1Value > p2Value) return 1;
+				return 0;
+			}
+			sort(products, productComparerByValue);
+			console.table(products);
+		});
+	});
+
+	describe('Any list by anything', function(){
+		function sort(list, comparer){
+			var comparerFn = comparer;
+			if (typeof comparer === 'string'){
+				comparerFn = function(item1, item2){
+					if (item1[comparer] < item2[comparer]) return -1;
+					if (item1[comparer] > item2[comparer]) return 1;
+					return 0;
+				}
+			}
+			for(var i=0; i < list.length-1; i++)
+				for(var j=i+1; j < list.length; j++)
+					if (comparerFn(list[i], list[j]) > 0){
+						var temp = list[i];
+						list[i] = list[j];
+						list[j] = temp;
+					}
+		}
+		describe('Products by category', function(){
+			sort(products, 'category');
+			console.table(products);
+		});
+		describe('Products by value [cost * units]', function(){
+			function productComparerByValue(p1, p2){
+				var p1Value = p1.cost * p1.units,
+					p2Value = p2.cost * p2.units;
+				if (p1Value < p2Value) return -1;
+				if (p1Value > p2Value) return 1;
+				return 0;
+			}
+			sort(products, productComparerByValue);
+			console.table(products);
+		});
+	});
+
+	
 });
 
-/*describe('Filter', function(){
-	describe('Filter costly products [cost > 50]', function(){
-		//filter();
-		console.table(products)
-	});
+describe('Filter', function(){
 	describe('Filter stationary products', function(){
-		//filter();
-		console.table(products)
-	});
-});*/
+		function filterStationaryProducts(){
+
+		}
+		var stationaryProducts = filterStationaryProducts();
+		console.table(stationaryProducts);
+	})
+	describe('Filter any list by any criteria', function(){
+		function filter(){
+
+		}
+		describe('Filter costly products [cost > 50]', function(){
+			var costlyProducts = filter(/*....*/);
+			console.table(products);
+		});
+		describe('Filter understocked products [units < 50]', function(){
+			var understockedProducts = filter(/*.....*/);
+			console.table(understockedProducts);
+		});
+	})
+});
